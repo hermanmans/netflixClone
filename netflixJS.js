dfd245195f3ea6fa65368f41d110003e39ad51d6
hermanmans <hsmans0601@gmail.com>
2022-07-18 14:58:37 +0200

    18/07 15:00

--- netflixJS.js
+++ netflixJS.js
@@ -9,3 +9,34 @@ function showBar(){
 }
 
 searchPic.addEventListener("click",showBar);
+
+////// Vue instance
+
+let vueObj = new Vue({
+  el:"#myApp",
+  data:{
+     state:"default",
+     newItem: "",
+     myList:[{label:"Walk the dog",done:false},
+              {label:"Walk the cat",done:true}]
+  },
+  methods:{
+      saveItem(){
+          this.myList.push({label:this.newItem,done:false});
+          this.newItem = ""; // This clears the input area after the submit or save button is pressed.
+      },
+
+      changeState(newState){// Toggle "Edit" and "Add Items" button
+          this.state = newState;
+      },
+
+      toggleStrike(x){
+          x.done = !x.done;
+      },
+  },
+  computed:{
+      reverseItems(){
+          return this.myList.slice(0).reverse(); //make copy of "myList" and reverse the order. Display will have to be updated in the list item.
+      }
+  }
+})
