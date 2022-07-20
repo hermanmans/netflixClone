8f2e5485b2aeac19d4270df927779d57a8770e61
hermanmans <hsmans0601@gmail.com>
2022-07-20 16:52:08 +0200

    20/07 16:52

---
+++ login.js
@@ -0,0 +1,24 @@
+//Pre-defined data (registered user)
+let userData = '{"user":"developer","password":"admin"}'
+localStorage.setItem("userLogin",userData);
+///
+let login = document.querySelector("#loginButton");
+let userObj = {};
+function saveLogin(){
+  let check = localStorage.getItem("userLogin");
+  let userInp = document.getElementById("user").value;
+  let passwordInp = document.getElementById("password").value;
+  userObj.user = userInp;
+  userObj.password =passwordInp;
+  console.log(userObj);
+  if(check===JSON.stringify(userObj)){
+    alert("CORRECT");
+    window.location = "http://127.0.0.1:5500/index.html";
+
+  }else{
+    alert("Username or password incorrect, try again");
+  }
+  
+}
+
+login.addEventListener("click",saveLogin);
