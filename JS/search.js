97d00117796721ab275e4eca3541c6576faee3f8
hermanmans <hsmans0601@gmail.com>
2022-09-12 12:52:17 +0200

    12/09
    Linked Video

--- JS/search.js
+++ JS/search.js
@@ -1,9 +1,10 @@
 let selection = JSON.parse(localStorage.getItem("movieData"));
   let searchVueObj = new Vue({
-    el:'#searchGrid',
+    el:'#moviePage',
     data:selection,
     methods:{
       searchMovie(var2){
+        window.location = "http://127.0.0.1:5500/HTML/search.html";
         let searchArr = [];
         this.data.forEach(element => {
           if(element == var2){
@@ -11,7 +12,6 @@ let selection = JSON.parse(localStorage.getItem("movieData"));
             searchArr.push(search);
             console.log(searchArr);
           }
-  
         })
       },
     }
